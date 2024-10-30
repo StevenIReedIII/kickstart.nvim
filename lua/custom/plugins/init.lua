090aafdf44119ff712ae10441feb2fa8be702bfe
Steven Reed <101155741+StevenIReedIII@users.noreply.github.com>
2024-10-30 10:44:07 -0500

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+local java_runner = require("custom.java_runner")
+
+vim.api.nvim_set_keymap("n", "<leader>jr", ":lua require('custom.java_runner').run_java_and_cleanup()<CR>", { noremap = true, silent = true })
+
+
 return {}
